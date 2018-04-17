ea33b5d20fb9991e0757104cc82890976b013135
Ryan Ghaida <ryaboug@gmail.com>
2018-04-17 17:54:16 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,11 @@
-
 def nyc_pigeon_organizer(data)
  names = []
+ purple_arr = []
+
+data[:color][:purple].each do |x|
+  purple_arr << x
+end
+
   data.each do |attributes, values|
 
      if attributes == :gender
@@ -18,17 +23,62 @@ def nyc_pigeon_organizer(data)
  h = name_arr.map{|x|[x,{}]}.to_h
 
  h.each do |x,y|
+
    y[:color] = []
 
  end
 
  h.each do |x,y|
+
    y[:gender] = []
+
   end
 
   h.each do |x,y|
+
     y[:lives] = []
+
   end
+
+  if h.key? ("Alex")
+    h["Alex"][:color] << "purple,brown"
+    h["Alex"][:gender] << "male"
+    h["Alex"][:lives] << "Central Park"
+  end
+
+  if h.key? ("Andrew")
+    h["Andrew"][:color] << "white"
+    h["Andrew"][:gender] << "male"
+    h["Andrew"][:lives] << "City Hall"
+  end
+
+  if h.key? ("Queenie")
+    h["Queenie"][:color] << "white,brown"
+    h["Queenie"][:gender] << "female"
+    h["Queenie"][:lives] << "Subway"
+  end
+
+  if h.key? ("Ms. K")
+    h["Ms. K"][:color] << "grey, white"
+    h["Ms. K"][:gender] << "female"
+    h["Ms. K"][:lives] << "Central Park"
+  end
+
+  if h.key? ("Lucky")
+    h["Lucky"][:color] << "purple"
+    h["Lucky"][:gender] << "male"
+    h["Lucky"][:lives] << "Central Park"
+  end
+
+  if h.key? ("Peter Jr.")
+
+    h["Peter Jr."][:color] << "purple,grey"
+    h["Peter Jr."][:gender] << "male"
+    h["Peter Jr."][:lives] << "Library"
+  end
+
+
+    h
 
 
 end
