8c6bc07976c03cbcc47b544187dd2b14b316cfca
Ryan Ghaida <ryaboug@gmail.com>
2018-04-17 17:08:14 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ names = [] 
+  data.each do |attributes, values|
+     
+     if attributes == :gender 
+       
+        values.each do |gender,pidgeon_names|
+           names << pidgeon_names 
+        end 
+  
+     end
+    
+  end 
+  
+  name_arr = names.flatten
+  
+ h = name_arr.map{|x|[x,{}]}.to_h 
+ 
+ h.each do |x,y|
+   y[:color] = []
+    
+ end 
+ 
+ h.each do |x,y|
+   y[:gender] = [] 
+  end 
+  
+  h.each do |x,y|
+    y[:lives] = [] 
+  end 
+ 
+ 
 end
